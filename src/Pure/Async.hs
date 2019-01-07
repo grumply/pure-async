9e2c32192f7a1c60da8768fe17f7d780378cd02d
Sean Mackenzie <sean@grump.ly>
2019-01-07 00:03:44 -0600

    Refine safety of component typing.

--- src/Pure/Async.hs
+++ src/Pure/Async.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE MagicHash #-}
-module Pure.Async where
+{-# LANGUAGE MagicHash, ScopedTypeVariables, AllowAmbiguousTypes #-}
+module Pure.Async (Async(..),async,asyncAs) where
 
 import Pure.Data.Default
 import Pure.Data.View
@@ -7,21 +7,42 @@ import Pure.Data.View.Patterns
 
 import Control.Concurrent
 import Control.Monad
+import Data.Typeable
 
 import GHC.Exts
 
-newtype Async a = Async (a,View)
+data Async a = Async (IO ()) View
 
-async :: IO () -> View -> View
-async x v = flip LibraryComponentIO (Async (x,v)) $ \self ->
+-- | Execute the given action asynchronously when the given View is rendered.
+-- Use as:
+--
+-- > async (Async someIO someView :: Async someType)
+--
+async :: Typeable a => Async a -> View
+async = LibraryComponentIO $ \self ->
   def
     { construct = return ()
     , executing = do
-        Async (x,_) <- ask self
+        Async x _ <- ask self
         void $ forkIO x
-    , receive = \(Async (x,v)) _ -> do
-        Async (x',_) <- ask self
+    , receive = \(Async x v) _ -> do
+        Async x' _ <- ask self
         when (isTrue# (reallyUnsafePtrEquality# x x')) x
         return ()
-    , render = \(Async (_,v)) _ -> v
+    , render = \(Async _ v) _ -> v
     }
+
+
+-- | A version of `async` that requires TypeApplications rather than an explicit
+-- data constructor + type signature.
+--
+-- Instead of:
+--
+-- > async (Async someIO someView :: Async someType)
+--
+-- Use this:
+--
+-- > asyncAs @someType someIO someView
+--
+asyncAs :: forall a. Typeable a => IO () -> View -> View
+asyncAs io v = async (Async io v :: Async a)
