42133b8ad8cc9eb7b90b9186f27a37f68e8cba5b
grumply <sean@grump.ly>
2019-01-09 05:01:12 -0600

    Fix receive bug. Make Async poly-kinded and add a poly-kinded variant of asyncAs.

--- src/Pure/Async.hs
+++ src/Pure/Async.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MagicHash, ScopedTypeVariables, AllowAmbiguousTypes #-}
+{-# LANGUAGE MagicHash, ScopedTypeVariables, AllowAmbiguousTypes, PolyKinds, TypeInType #-}
 module Pure.Async (Async(..),async,asyncAs) where
 
 import Pure.Data.Default
@@ -7,31 +7,30 @@ import Pure.Data.View.Patterns
 
 import Control.Concurrent
 import Control.Monad
+import Data.Kind
 import Data.Typeable
-
 import GHC.Exts
 
-data Async a = Async (IO ()) View
+data Async (a :: k) = Async (IO ()) View
 
 -- | Execute the given action asynchronously when the given View is rendered.
 -- Use as:
 --
--- > async (Async someIO someView :: Async someType)
+-- > async (Async someIO someView :: Async someTypeOrKind)
 --
-async :: Typeable a => Async a -> View
+async :: forall k a. (Typeable (a :: k), Typeable k) => Async a -> View
 async = LibraryComponentIO $ \self ->
   def
     { construct = return ()
     , executing = do
         Async x _ <- ask self
         void $ forkIO x
-    , receive = \(Async x v) _ -> do
-        Async x' _ <- ask self
-        when (isTrue# (reallyUnsafePtrEquality# x x')) x
+    , receive = \(Async new v) _ -> do
+        Async old _ <- ask self
+        unless (isTrue# (reallyUnsafePtrEquality# new old)) new
         return ()
     , render = \(Async _ v) _ -> v
     }
-
 
 -- | A version of `async` that requires TypeApplications rather than an explicit
 -- data constructor + type signature.
@@ -44,5 +43,23 @@ async = LibraryComponentIO $ \self ->
 --
 -- > asyncAs @someType someIO someView
 --
-asyncAs :: forall a. Typeable a => IO () -> View -> View
+-- For a poly-kinded version, use asyncAs'
+asyncAs :: forall a. (Typeable (a :: *)) => IO () -> View -> View
 asyncAs io v = async (Async io v :: Async a)
+
+-- | A version of `async` that requires TypeApplications rather than an explicit
+-- data constructor + type signature.
+--
+-- This version of `async` allows for poly-kinded Async types.
+--
+-- Instead of:
+--
+-- > async (Async someIO someView :: Async somePolyKind)
+--
+-- Use this:
+--
+-- > asyncAs @_ @somePolyKind someIO someView
+--
+asyncAs' :: forall k a. (Typeable (a :: k), Typeable k) => IO () -> View -> View
+asyncAs' io v = async (Async io v :: Async a)
+
