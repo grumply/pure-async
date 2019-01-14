d8354460c6c7d8c34732c1681d4ecd627ab3781f
grumply <sean@grump.ly>
2019-01-14 06:03:35 -0600

    Refine async implementation.

--- src/Pure/Async.hs
+++ src/Pure/Async.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE MagicHash, ScopedTypeVariables, AllowAmbiguousTypes, PolyKinds, TypeInType #-}
-module Pure.Async (Async(..),async,asyncAs) where
+{-# LANGUAGE MagicHash, ScopedTypeVariables, AllowAmbiguousTypes, TypeApplications, RecordWildCards #-}
+module Pure.Async (async, asyncOnce, asyncOnceAs) where
 
 import Pure.Data.Default
 import Pure.Data.View
@@ -11,55 +11,59 @@ import Data.Kind
 import Data.Typeable
 import GHC.Exts
 
-data Async (a :: k) = Async (IO ()) View
+data Async a = Async
+  { action :: (IO ())
+  , view :: View
+  , once :: Bool
+  }
 
--- | Execute the given action asynchronously when the given View is rendered.
--- Use as:
---
--- > async (Async someIO someView :: Async someTypeOrKind)
---
-async :: forall k a. (Typeable (a :: k), Typeable k) => Async a -> View
-async = LibraryComponentIO $ \self ->
+async_ :: Typeable a => Async a -> View
+async_ = LibraryComponentIO $ \self ->
   def
-    { construct = return ()
-    , executing = do
-        Async x _ <- ask self
-        void $ forkIO x
-    , receive = \(Async new v) _ -> do
-        Async old _ <- ask self
-        unless (isTrue# (reallyUnsafePtrEquality# new old)) new
-        return ()
-    , render = \(Async _ v) _ -> v
+    { construct = do
+        Async {..} <- ask self
+        forkIO action
+    , receive = \Async { action = new, .. } tid -> do
+        Async { action = old } <- ask self
+        if not once && not (isTrue# (reallyUnsafePtrEquality# new old)) then do
+          killThread tid
+          forkIO new
+        else do
+          return tid
+    , render = \Async {..} _ -> view
+    , unmounted = do
+        tid <- get self
+        killThread tid
     }
 
--- | A version of `async` that requires TypeApplications rather than an explicit
--- data constructor + type signature.
+-- | Execute the given action asynchronously before the given View is first
+-- rendered or when the action changes.
 --
--- Instead of:
+-- Use as:
 --
--- > async (Async someIO someView :: Async someType)
+-- > async someIOAction someView
 --
--- Use this:
---
--- > asyncAs @someType someIO someView
---
--- For a poly-kinded version, use asyncAs'
-asyncAs :: forall a. (Typeable (a :: *)) => IO () -> View -> View
-asyncAs io v = async (Async io v :: Async a)
+async :: IO () -> View -> View
+async action view = async_ (Async action view False :: Async ())
 
--- | A version of `async` that requires TypeApplications rather than an explicit
--- data constructor + type signature.
---
--- This version of `async` allows for poly-kinded Async types.
---
--- Instead of:
---
--- > async (Async someIO someView :: Async somePolyKind)
---
--- Use this:
---
--- > asyncAs @_ @somePolyKind someIO someView
---
-asyncAs' :: forall k a. (Typeable (a :: k), Typeable k) => IO () -> View -> View
-asyncAs' io v = async (Async io v :: Async a)
 
+-- | Execute the given action asynchronously before the given View is first
+-- rendered.
+--
+-- Use as:
+--
+-- > asyncOnce someIOAction someView
+--
+asyncOnce :: IO () -> View -> View
+asyncOnce = asyncOnceAs @()
+
+-- | Execute the given action asynchronously before the given View is first
+-- rendered. Requires a visible type application for some hairy situations
+-- that require special-case diffing of async once views.
+--
+-- Use as:
+--
+-- > asyncOnceAs @SomeType someIOAction someView
+--
+asyncOnceAs :: forall a. Typeable a => IO () -> View -> View
+asyncOnceAs action view = async_ (Async action view True :: Async a)
